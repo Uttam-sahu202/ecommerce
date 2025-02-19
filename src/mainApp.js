2dfaded2dbda0ee88af387ee44e03b5af78efb95
uttam sahu <uttam.sahu@housing.com>
2025-02-19 18:31:35 +0530

    cart button done

--- src/mainApp.js
+++ src/mainApp.js
@@ -2,37 +2,46 @@ import DetailPage from "./components/DetailPage";
 import HomePage from './components/HomePage';
 import SearchPage from './components/SearchPage';
 import CartPage from "./components/CartPage.js"
-import { Routes, Route, useNavigate } from "react-router-dom"; 
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { connect } from "react-redux";
 
-const  MainApp = () =>{             // inject the nomber of array you have in the cart and show this above cart 
-    const navigate = useNavigate(); 
-  
+const MainApp = ({ numberOfItemInCart }) => {              
+  const navigate = useNavigate();
 
-    const handleCartButton = () =>{
-      navigate("/cart");
-    }
-  
-    return (
-      <>
-        
-        <header className="inputSection">
-          <div>
-          wellcome to ecommerce ...
-          </div>
-          <div>
-            <button onClick={() => handleCartButton()}>Cart</button>
-          </div>
-        </header>
-  
-        
-        <Routes>
-          <Route path="/" element={<HomePage />} /> 
-          <Route path="/search/:searchedItem" element={<SearchPage />} /> 
-          <Route path="/detail/:id" element={<DetailPage />} />
-          <Route path="/cart" element={<CartPage />} />
-        </Routes>
-      </>
-    );
+
+
+
+  const handleCartButton = () => {
+    navigate("/cart");
   }
 
-  export default MainApp;
+  return (
+    <>
+
+      <header className="inputSection">
+        <div>
+          wellcome to ecommerce ...
+        </div>
+        <div>
+          <button className="cart-button" onClick={() => handleCartButton()}>
+            Cart{numberOfItemInCart > 0 && <span className="cartValue">{numberOfItemInCart}</span>}
+          </button>
+        </div>
+      </header>
+
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search/:searchedItem" element={<SearchPage />} />
+        <Route path="/detail/:id" element={<DetailPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </>
+  );
+}
+
+
+const mapStateToProps = (state) => ({
+  numberOfItemInCart: state.cartReducer.length,
+});
+export default connect(mapStateToProps, null)(MainApp);
